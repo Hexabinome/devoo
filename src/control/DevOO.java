d49630fca5d63e5918b819a2517f903ef0b9e8ac
maxou42 <maxinsa42@gmail.com>
2015-11-11 12:41:52 +0100

    added git import test message

--- src/control/DevOO.java
+++ src/control/DevOO.java
@@ -12,7 +12,7 @@ public class DevOO
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+    	System.out.println("felicitations ton git import marche.");
     }
     
 }
