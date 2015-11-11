131eac4aa3c185308ce11a3e4999901f863e0c67
maxou42 <maxinsa42@gmail.com>
2015-11-11 12:17:58 +0100

    Initial commit. "src" and "lib" folders of a initial NetBeans project. Feel free to use the IDE of your choice.
    package folders. (so far model view control)

---
+++ README.md
@@ -0,0 +1,6 @@
+Hi,
+
+the repo content are "src" and "lib" folder of NetBeans project. "src" contains java's package structure, so you should be able to use
+pretty any IDE you want. However the repo will not manage IDE settings.
+
+[Maxou]
---
+++ src/control/DevOO.java
@@ -0,0 +1,18 @@
+package control;
+
+/**
+ * Base control class for the DevOO project
+ * @author Maximilian Schiedermeier
+ */
+public class DevOO
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
