4dcc9ddcac13bc41e3e366fa99896b10718545aa
Mohamed Haidara <mohamed.haidara@insa-lyon.fr>
2015-11-12 15:10:40 +0100

    Move sample.fxml to ressource

--- src/main/java/controleur/DevOO.java
+++ src/main/java/controleur/DevOO.java
@@ -23,7 +23,8 @@ public class DevOO extends Application
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
