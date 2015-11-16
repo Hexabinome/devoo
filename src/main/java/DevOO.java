6a5fad7cb80934dce87a3264f90ea799e72b272e
Mohamed Haidara <mohamed.haidara@insa-lyon.fr>
2015-11-16 20:42:50 +0100

    Implementation d'une première fenetre

--- src/main/java/DevOO.java
+++ src/main/java/DevOO.java
@@ -1,20 +1,18 @@
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import org.jdom2.JDOMException;
 import org.xml.sax.SAXException;
-
 import xml.DeserialiseurXML;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 /**
- * Base control class for the DevOO project
+ * Point d'entrée de l'application
  *
  * @author Maximilian Schiedermeier
  */
@@ -24,24 +22,28 @@ public class DevOO extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //launch(args);
-        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        launch(args);
+        /*ClassLoader classLoader = ClassLoader.getSystemClassLoader();
         try {
             DeserialiseurXML.ouvrirPlanDeVille(new File(classLoader.getResource("samples/plan20x20.xml").toURI()));
-        } catch (SAXException | IOException  | URISyntaxException e) {
+        } catch (SAXException | IOException | URISyntaxException e) {
             e.printStackTrace();
         } catch (JDOMException e) {
-			System.err.println("Pas valide car : ");
-			System.err.println(e.getLocalizedMessage());
-		}
+            System.err.println("Pas valide car : ");
+            System.err.println(e.getLocalizedMessage());
+        }*/
+
     }
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/RootLayout.fxml"));
         primaryStage.setTitle("OptimodLyon");
-        primaryStage.setScene(new Scene(root, 1200, 768));
+        primaryStage.setScene(new Scene(root, 1000, 668));
+        primaryStage.setMinWidth(1000);
+        primaryStage.setMinHeight(668);
         primaryStage.show();
     }
 }
