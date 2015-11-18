9aa8f7f3f39d11078f8009d2507ad68cb136b4ad
robinroyer <robin.royer974@gmail.com>
2015-11-18 16:58:25 +0100

    adding model unit test file

---
+++ src/test/java/IntersectionTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author robinroyer
+ */
+public class IntersectionTest {
+    
+        //-----------TEST  d'intersection 
+        /*  +Intersection(id:int,x:int,y:int)
+            +addTroncon(int id, Troncon troncon):void
+            +getId():int
+            +getX():int
+            +getY():int
+            +getTroncon(cibleId:int):Troncon
+            0 aLiaison(cibleId:int):boolean
+            +toString():String
+        */
+    
+}
---
+++ src/test/java/TronconTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author robinroyer
+ */
+public class TronconTest {
+    
+        //-----------TEST  troncon 
+        /*
+            +Troncon(String nomRue, float vitesse, float longueur, int idDestination): ctor
+            +getNomRue(): String
+            +setNomRue(String nomRue): void
+            +getVitesse(): float
+            +getLongueur(): float
+            +getDuree(): float
+            +toString(): String
+        */
+    
+}
