fec201fb52b8854a364784030952d8ccd6d3b103
robinroyer <robin.royer974@gmail.com>
2015-11-21 20:36:16 +0100

    completing XMLdeserialiseur test, TODO : uncomment and finish the iteration over the fenetre of a demande

--- src/test/java/DeserialiseurXMLTest.java
+++ src/test/java/DeserialiseurXMLTest.java
@@ -1,10 +1,11 @@
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
 
 import modele.persistance.DeserialiseurXML;
 import modele.xmldata.Demande;
-import modele.xmldata.Model;
-import modele.xmldata.ModelLecture;
+import modele.xmldata.Fenetre;
+import modele.xmldata.Livraison;
 import modele.xmldata.PlanDeVille;
 import modele.xmldata.Troncon;
 
@@ -27,24 +28,69 @@ public class DeserialiseurXMLTest {
       PlanDeVille ville = DeserialiseurXML.ouvrirPlanDeVille(ClassLoader.getSystemResourceAsStream("samples/planTestLight.xml"));
 
       //init troncons
-      Troncon troncon1 = new Troncon("v0",4,150,1);
-      Troncon troncon2 = new Troncon("v0",5,100,4);
-      Troncon troncon3 = new Troncon("v0",6,200,6);
-      Troncon troncon4 = new Troncon("v0",7,300,9);
+      Troncon troncon15 = new Troncon("r0",1,150,5);
+      Troncon troncon45 = new Troncon("v0",2,100,5);
+      Troncon troncon65 = new Troncon("q0",7,200,5);
+      Troncon troncon95 = new Troncon("h0",8,300,5);
+      Troncon troncon51 = new Troncon("v0",3,150,1);
+      Troncon troncon54 = new Troncon("v0",4,100,4);
+      Troncon troncon56 = new Troncon("v0",5,200,6);
+      Troncon troncon59 = new Troncon("v0",6,300,9);
 
-      //test de l'intersection stockees
-      assertEquals("La coordonnee en x de l'intersection doit etre recuperer", 20, ville.getIntersection(5).getX());
-      assertEquals("La coordonnee en y de l'intersection doit etre recuperer", 30, ville.getIntersection(5).getY());
+      //test des coordonnes de la premiere et derniere intersection
+      assertEquals("La coordonnee en x de l'intersection 5 doit etre stockee", 20, ville.getIntersection(1).getX());
+      assertEquals("La coordonnee en y de l'intersection 5 doit etre stockee", 15, ville.getIntersection(1).getY());
+      assertEquals("La coordonnee en x de l'intersection doit etre stockee", 20, ville.getIntersection(9).getX());
+      assertEquals("La coordonnee en y de l'intersection doit etre stockee", 60, ville.getIntersection(9).getY());
+      //test de presence de toutes les intersections
       assertEquals("L'id de l'intersection doit etre correcte", 1, ville.getIntersection(1).getId());
       assertEquals("L'id de l'intersection doit etre correcte", 4, ville.getIntersection(4).getId());
       assertEquals("L'id de l'intersection doit etre correcte", 5, ville.getIntersection(5).getId());
       assertEquals("L'id de l'intersection doit etre correcte", 6, ville.getIntersection(6).getId());
       assertEquals("L'id de l'intersection doit etre correcte", 9, ville.getIntersection(9).getId());
-      //test des troncons lies a l'intersection
-      assertEquals("L'intersection stockees doit avoir ses troncons", troncon1, ville.getIntersection(5).getTroncon(1));
-      assertEquals("L'intersection stockees doit avoir ses troncons", troncon2, ville.getIntersection(5).getTroncon(4));
-      assertEquals("L'intersection stockees doit avoir ses troncons", troncon3, ville.getIntersection(5).getTroncon(6));
-      assertEquals("L'intersection stockees doit avoir ses troncons", troncon4, ville.getIntersection(5).getTroncon(9));
+      //test des nom de troncons partant de l'intersection 5
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon51.getNomRue(), ville.getIntersection(5).getTroncon(1).getNomRue());
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon54.getNomRue(), ville.getIntersection(5).getTroncon(4).getNomRue());
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon56.getNomRue(), ville.getIntersection(5).getTroncon(6).getNomRue());
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon59.getNomRue(), ville.getIntersection(5).getTroncon(9).getNomRue());
+      //test des vitesses de troncons partant de l'intersection 5
+     assertEquals("L'intersection stockees doit avoir ses troncons", troncon51.getVitesse(), ville.getIntersection(5).getTroncon(1).getVitesse(), 0);
+     assertEquals("L'intersection stockees doit avoir ses troncons", troncon54.getVitesse(), ville.getIntersection(5).getTroncon(4).getVitesse(), 0);
+     assertEquals("L'intersection stockees doit avoir ses troncons", troncon56.getVitesse(), ville.getIntersection(5).getTroncon(6).getVitesse(), 0);
+     assertEquals("L'intersection stockees doit avoir ses troncons", troncon59.getVitesse(), ville.getIntersection(5).getTroncon(9).getVitesse(), 0);
+      //test des longueurs de troncons partant de  l'intersection 5
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon51.getLongueur(), ville.getIntersection(5).getTroncon(1).getLongueur(), 0);
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon54.getLongueur(), ville.getIntersection(5).getTroncon(4).getLongueur(), 0);
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon56.getLongueur(), ville.getIntersection(5).getTroncon(6).getLongueur(), 0);
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon59.getLongueur(), ville.getIntersection(5).getTroncon(9).getLongueur(), 0);
+      //test des durees de troncons partant de l'intersection 5
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon51.getDuree(), ville.getIntersection(5).getTroncon(1).getDuree(),0);
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon54.getDuree(), ville.getIntersection(5).getTroncon(4).getDuree(),0);
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon56.getDuree(), ville.getIntersection(5).getTroncon(6).getDuree(),0);
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon59.getDuree(), ville.getIntersection(5).getTroncon(9).getDuree(),0);
+      //test des noms de troncons lies a l'intersection 5
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon15.getNomRue(), ville.getIntersection(1).getTroncon(5).getNomRue());
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon45.getNomRue(), ville.getIntersection(4).getTroncon(5).getNomRue());
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon65.getNomRue(), ville.getIntersection(6).getTroncon(5).getNomRue());
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon95.getNomRue(), ville.getIntersection(9).getTroncon(5).getNomRue());
+
+      //test des vitesse de troncons lies a l'intersection 5
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon15.getVitesse(), ville.getIntersection(1).getTroncon(5).getVitesse(),0);
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon45.getVitesse(), ville.getIntersection(4).getTroncon(5).getVitesse(),0);
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon65.getVitesse(), ville.getIntersection(6).getTroncon(5).getVitesse(),0);
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon95.getVitesse(), ville.getIntersection(9).getTroncon(5).getVitesse(),0);
+
+      //test des longueurs de troncons lies a l'intersection 5
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon15.getLongueur(), ville.getIntersection(1).getTroncon(5).getLongueur(), 0);
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon45.getLongueur(), ville.getIntersection(4).getTroncon(5).getLongueur(), 0);
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon65.getLongueur(), ville.getIntersection(6).getTroncon(5).getLongueur(), 0);
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon95.getLongueur(), ville.getIntersection(9).getTroncon(5).getLongueur(), 0);
+
+      //test des durees de troncons lies a l'intersection 5
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon15.getDuree(), ville.getIntersection(1).getTroncon(5).getDuree(),0);
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon45.getDuree(), ville.getIntersection(4).getTroncon(5).getDuree(),0);
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon65.getDuree(), ville.getIntersection(6).getTroncon(5).getDuree(),0);
+      assertEquals("L'intersection stockees doit avoir ses troncons", troncon95.getDuree(), ville.getIntersection(9).getTroncon(5).getDuree(),0);
     }
 
 
@@ -53,7 +99,22 @@ public class DeserialiseurXMLTest {
       PlanDeVille ville = DeserialiseurXML.ouvrirPlanDeVille(ClassLoader.getSystemResourceAsStream("samples/planTest.xml"));
       Demande demande = DeserialiseurXML.ouvrirLivraison(ClassLoader.getSystemResourceAsStream("samples/livraisonTest.xml"), ville);
 
-      assertEquals("fail", 0, demande);
+      Livraison livr1 = new Livraison(1, 1, 1);
+      Livraison livr2 = new Livraison(2, 2, 9);
+      Livraison livr3 = new Livraison(3, 3, 4);
+
+      //test si l'entrepot a le bon id d'intercetion
+      assertEquals("l'entrepot doit etre stocke", 5, demande.getEntrepot().getId());
+      //test si les fenetres sont bien stockees
+      // assertEquals("La fenetre 1 doit avoir le bon timestamp de debut", 8 * 3600, ((Fenetre)((ArrayList <Fenetre>)demande.getFenetres()).get(0)).getTimestampDebut());
+      // assertEquals("La fenetre 1 doit avoir le bon timestamp de fin", 12 * 3600, ((Fenetre)((ArrayList <Fenetre>)demande.getFenetres()).get(0)).getTimestampFin());
+
+      // assertEquals("La fenetre 2 doit avoir le bon timestamp de debut", 13 * 3600, ((Fenetre)((ArrayList <Fenetre>)demande.getFenetres()).get(1)).getTimestampDebut());
+      // assertEquals("La fenetre 2 doit avoir le bon timestamp de fin", 18 * 3600, ((Fenetre)((ArrayList <Fenetre>)demande.getFenetres()).get(1)).getTimestampFin());
+      // //test si les livraison sont bien stockees
+      // assertEquals("La livraison 1 doit etre stockee", livr1, ((Fenetre)((ArrayList <Fenetre>)demande.getFenetres()).get(0)).getLivraisons().get(1));
+      // assertEquals("La livraison 2 doit etre stockee", livr2, ((Fenetre)((ArrayList <Fenetre>)demande.getFenetres()).get(0)).getLivraisons().get(2));
+      // assertEquals("La livraison 3 doit etre stockee", livr3, ((Fenetre)((ArrayList <Fenetre>)demande.getFenetres()).get(1)).getLivraisons().get(3));
     }
 
 
