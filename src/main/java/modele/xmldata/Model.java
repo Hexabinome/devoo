4c21061b66ba3b2efe3405193df2203122e2564b
maxou42 <maxinsa42@gmail.com>
2015-11-22 13:49:23 +0100

    prepared methods needed to add / delete deliveries to the model

--- src/main/java/modele/xmldata/Model.java
+++ src/main/java/modele/xmldata/Model.java
@@ -40,12 +40,29 @@ public class Model implements ModelLecture
         return demande;
     }
 
+    public void removeLivraison(int id)
+    {
+        //TODO: (cette methode ne fait pas parti du noyau... pas prioritaire maintenant.)
+        // Quoi on doit faire ici:
+        // + effacer la livraison dans demande
+        // + mis a jour du graphe (supprimer les liasion qui utilisent l'intersection utilise pas la livraison)
+        // + soit (1) effacer tournee / soit (2) recalculer tournee avec TSP -> encore a discuter mais a mon avis (2)
+    }
+
+    public void addLivraison(int previousId, int intersectionId)
+    {
+        //TODO: (cette methode ne fait pas parti du noyau... pas prioritaire maintenant.)
+        // Quoi on doit faire ici:
+        // + ajouter la livraison dans la bonne fenter dans demande
+        // + mis a jour du graphe (calculer distance vers chque intersection deja utilise pour une livraison dans graphe)
+        // + soit (1) effacer tournee / soit (2) recalculer tournee avec TSP -> encore a discuter mais a mon avis (2)
+    }
+
     @Override
     public void calculerTournee()
     {
         demande.creerGraphe(plan);
-        
+
         //TODO: apres avoir calcule le graphe il faut appeler TSP ici.
     }
-
 }
