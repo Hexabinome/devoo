b1e11139883df3267ff4302701fb8445e67d21c2
maxou42 <maxinsa42@gmail.com>
2015-11-23 14:38:26 +0100

    refactores dijkstra in fenetre but still not working

--- src/main/java/modele/xmldata/Fenetre.java
+++ src/main/java/modele/xmldata/Fenetre.java
@@ -22,6 +22,8 @@ public class Fenetre implements Visitable
 
     private final int heureDebut;
     private final int heureFin;
+
+    // map qui stoque des livraisons idntifie evec leur livraisonId
     private final Map<Integer, Livraison> livraisons;
 
     public Fenetre(int timestampDebut, int timestampFin)
@@ -75,86 +77,80 @@ public class Fenetre implements Visitable
      * @param graphe in/out
      * @param fNext
      */
-	public void calculerChemins(PlanDeVille plan, GrapheRealisation graphe, Fenetre fNext) 
-	{
-		//Récupère toutes les livraisons avec les quels on doit calculer le plus court chemin.
-		Set<Integer> intersectionsRecherchee = new HashSet<Integer>();
-		intersectionsRecherchee.addAll(livraisons.keySet());
-		intersectionsRecherchee.addAll(fNext.getLivraisons().keySet());
-		
-		for(Map.Entry<Integer, Livraison> livraison : livraisons.entrySet())
-		{
-			//Recupration de l'intersection de la livraison
-			Intersection intersection = plan.getIntersection(livraison.getKey());
-			
-			//Execute l'algorithme et ajoute dans la matrice les chemins
-			for(Entry<Integer, Chemin> chemin : dijkstra(intersection, plan))
-			{
-				if(intersectionsRecherchee.contains(chemin.getKey()))
-					graphe.setChemin(chemin.getValue());
-			}
-		}
-	}
-	
-	//TODO set private (public pour les test)
-	//TODO optimisation : quand on a calculer tous les chemins vers la fenêtre suivantes, on arête.  
-	public Set<Entry<Integer, Chemin>> dijkstra(Intersection intersectionDepart, PlanDeVille plan)
-	{
-		//INITIALISATION
-		
-		//Map de chemin intermédiaires et finaux
-			//La clé est l'id de l'intersection au quel correspond le chemin 
-			//le chemin contient tous tronçons par lesquels on est passé 
-			//+ l'id de l'interserction de départ et la clef de la map comme intersection d'arrivé
-		//TODO changer en set
+    public void calculerChemins(PlanDeVille plan, GrapheRealisation graphe, Fenetre fNext)
+    {
+        //Récupère toutes les livraisons avec les quels on doit calculer le plus court chemin.
+        Set<Integer> intersectionsRecherchee = new HashSet<>();
+        intersectionsRecherchee.addAll(livraisons.keySet());
+        intersectionsRecherchee.addAll(fNext.getLivraisons().keySet());
 
+        // pour chaque livraison de cette fenetre:
+        for (Livraison livraison : livraisons.values()) {
+            // recupere l'intersection
+            Intersection intersection = plan.getIntersection(livraison.getAdresse());
+
+            // utilise dijkstra pour calculer le plus court chemin vers chaque intersection...
+            for (Entry<Integer, Chemin> chemin : dijkstra(intersection, plan)) {
+                // ... et stoque le chemin ssi l'intersection correspond a une livrasion de cette ou la prochaine fenetre
+                if (intersectionsRecherchee.contains(chemin.getKey()))
+                    graphe.setChemin(chemin.getValue());
+            }
+        }
+    }
+
+    //TODO set private (public pour les test)
+    //TODO optimisation : quand on a calculer tous les chemins vers la fenêtre suivantes, on arête.  
+    public Set<Entry<Integer, Chemin>> dijkstra(Intersection intersectionDepart, PlanDeVille plan)
+    {
+		//INITIALISATION
+
+        //Map de chemin intermédiaires et finaux
+        //La clé est l'id de l'intersection au quel correspond le chemin 
+        //le chemin contient tous tronçons par lesquels on est passé 
+        //+ l'id de l'interserction de départ et la clef de la map comme intersection d'arrivé
+        //TODO changer en set
         Map<Integer, Chemin> chemins = new HashMap<>();
         chemins.put(intersectionDepart.getId(),
-                new Chemin(0, new ArrayList<Troncon>(), intersectionDepart.getId(), intersectionDepart.getId()));
+                new Chemin(0, new ArrayList<>(), intersectionDepart.getId(), intersectionDepart.getId()));
 
         Comparator<Intersection> comparator = new CoutComparator();
-        PriorityQueue<Intersection> queue = new PriorityQueue<Intersection>(1000, comparator);
+        PriorityQueue<Intersection> queue = new PriorityQueue<>(1000, comparator);
         queue.add(intersectionDepart);
 
         //ALGO
-        while(!queue.isEmpty())
-        {
-        	Intersection intersection = queue.poll();
-        	
-        	//Pour toutes les intersections suivantes
-    		for(Intersection intersectionSuivante : getListeIntersectionSuivante(intersection, plan))
-    		{
-    			//On récupére le chemin en cours de contruction dans la map
-    			Chemin chemin = chemins.get(intersection.getId());
-    			
-    			//Calcule d'information pour le nouveau chemin
-    			//On créer une nouvelle arrayList pour ne pas modifier celles qu'on récupères
-    			ArrayList<Troncon> listeTronconsEnCours = new ArrayList<Troncon>();
-    			listeTronconsEnCours.addAll(chemin.getTroncons());
-    			
-    			//Tronçon qu'on va traverser en allant au suivant
-    			Troncon tronconTraverser = intersection.getTroncon(intersectionSuivante.getId());
-    			listeTronconsEnCours.add(tronconTraverser);
-    			
-    			float cout = chemin.getCout() + tronconTraverser.getCout();
-    			
-    			//Insertion ou remplacement si le cout est inf�rieur du nouveau chemin dans la map
-    			Chemin cheminDejaInserer = chemins.get(intersectionSuivante.getId());
-    			if(cheminDejaInserer != null)
-    			{
-    				if(cheminDejaInserer.getCout() > cout)
-    				{	chemins.put(intersectionSuivante.getId(), 
-            					new Chemin(cout, listeTronconsEnCours, chemin.getIdDepart(), intersectionSuivante.getId()));
-    				}
-    			}
-    			else
-    			{
-    				queue.add(intersectionSuivante);
-    				chemins.put(intersectionSuivante.getId(), 
-        					new Chemin(cout, listeTronconsEnCours, chemin.getIdDepart(), intersectionSuivante.getId()));
-    			}
-    		}
-    		//intersection est maintenant noir. On peut maintenant le pop de la liste des intersection recherché
+        while (!queue.isEmpty()) {
+            Intersection intersection = queue.poll();
+
+            //Pour toutes les intersections suivantes
+            for (Intersection intersectionSuivante : getListeIntersectionSuivante(intersection, plan)) {
+                //On récupére le chemin en cours de contruction dans la map
+                Chemin chemin = chemins.get(intersection.getId());
+
+                //Calcule d'information pour le nouveau chemin
+                //On créer une nouvelle arrayList pour ne pas modifier celles qu'on récupères
+                ArrayList<Troncon> listeTronconsEnCours = new ArrayList<>();
+                listeTronconsEnCours.addAll(chemin.getTroncons());
+
+                //Tronçon qu'on va traverser en allant au suivant
+                Troncon tronconTraverser = intersection.getTroncon(intersectionSuivante.getId());
+                listeTronconsEnCours.add(tronconTraverser);
+
+                float cout = chemin.getCout() + tronconTraverser.getCout();
+
+                //Insertion ou remplacement si le cout est inf�rieur du nouveau chemin dans la map
+                Chemin cheminDejaInserer = chemins.get(intersectionSuivante.getId());
+                if (cheminDejaInserer != null) {
+                    if (cheminDejaInserer.getCout() > cout)
+                        chemins.put(intersectionSuivante.getId(),
+                                new Chemin(cout, listeTronconsEnCours, chemin.getIdDepart(), intersectionSuivante.getId()));
+                }
+                else {
+                    queue.add(intersectionSuivante);
+                    chemins.put(intersectionSuivante.getId(),
+                            new Chemin(cout, listeTronconsEnCours, chemin.getIdDepart(), intersectionSuivante.getId()));
+                }
+            }
+            //intersection est maintenant noir. On peut maintenant le pop de la liste des intersection recherché
 
         }
 
