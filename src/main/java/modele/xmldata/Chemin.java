a6fdd12dc4bbeb7c3c48621d8c8bb57bdb82b645
maxou42 <maxinsa42@gmail.com>
2015-11-23 09:25:25 +0100

    refactoring

--- src/main/java/modele/xmldata/Chemin.java
+++ src/main/java/modele/xmldata/Chemin.java
@@ -1,47 +1,57 @@
 package modele.xmldata;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class Chemin {
-	private float cout;
-    private ArrayList<Troncon> troncons;
-    private int idDepart;
-    private int idFin;
-    
-	public Chemin(float cout, ArrayList<Troncon> troncons, int idDepart, int idFin) {
-		super();
-		this.cout = cout;
-		this.troncons = troncons;
-		this.idDepart = idDepart;
-		this.idFin = idFin;
-		
-		troncons = new ArrayList<Troncon>();
-	}
-	public float getCout() {
-		return cout;
-	}
-	public void setCout(float cout) {
-		this.cout = cout;
-	}
-	public List<Troncon> getTroncons() {
-		return troncons;
-	}
-	public void setTroncons(ArrayList<Troncon> troncons) {
-		this.troncons = troncons;
-	}
-	public int getIdDepart() {
-		return idDepart;
-	}
-	public void setIdDepart(int idDepart) {
-		this.idDepart = idDepart;
-	}
-	public int getIdFin() {
-		return idFin;
-	}
-	public void setIdFin(int idFin) {
-		this.idFin = idFin;
-	}
-    
-    
+public class Chemin
+{
+
+    private float cout;
+    private final ArrayList<Troncon> troncons;
+    private final int idDepart;
+    private final int idFin;
+
+    public Chemin(float cout, ArrayList<Troncon> troncons, int idDepart, int idFin)
+    {
+        super();
+        this.cout = cout;
+        this.troncons = troncons;
+        this.idDepart = idDepart;
+        this.idFin = idFin;
+
+        troncons = new ArrayList<>();
+    }
+
+    public float getCout()
+    {
+        return cout;
+    }
+
+    public void setCout(float cout)
+    {
+        this.cout = cout;
+    }
+
+    public List<Troncon> getTroncons()
+    {
+        return Collections.unmodifiableList(troncons);
+    }
+
+    /*
+    public void setTroncons(ArrayList<Troncon> troncons)
+    {
+        this.troncons = troncons;
+    }*/
+
+    public int getIdDepart()
+    {
+        return idDepart;
+    }
+
+    public int getIdFin()
+    {
+        return idFin;
+    }
+
 }
