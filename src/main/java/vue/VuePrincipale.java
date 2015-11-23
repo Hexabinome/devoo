edcd060956f3c9f10517d6d5a6f8a00cc5aba6b7
David-Wobrock <david.wobrock@gmail.com>
2015-11-23 09:10:34 +0100

    refactor code pour le côté graphique

--- src/main/java/vue/VuePrincipale.java
+++ src/main/java/vue/VuePrincipale.java
@@ -2,10 +2,6 @@ package vue;
 
 import java.io.File;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
@@ -14,20 +10,12 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Menu;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Ellipse;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Polygon;
 import javafx.stage.FileChooser;
-import javafx.util.Pair;
-import modele.xmldata.Intersection;
-import modele.xmldata.PlanDeVille;
 
 import org.controlsfx.dialog.ExceptionDialog;
 
@@ -45,17 +33,6 @@ public class VuePrincipale implements Initializable {
      * Mediateur : permet de communiquer avec les autres controleurs
      */
     private FenetrePrincipale mediateur;
-
-    /**
-     * La taille sur l'interface graphique d'une intersection du plan de la
-     * ville
-     */
-    private final double DIAMETRE_INTERSECTION = 7;
-    /**
-     * La marge à laisser sur les côté du canvas graphique afin d'avoir plus du
-     * lisibilité
-     */
-    private final int MARGE_INTERSECTION = 30;
 
     /**
      * Largeur de la boîte de dialogue d'erreur
@@ -76,6 +53,8 @@ public class VuePrincipale implements Initializable {
      */
     @FXML
     private Pane canvasGraphique;
+    
+    private VueGraphiqueAideur vueGraphique;
 
     /**
      * Le menu en haut
@@ -107,23 +86,7 @@ public class VuePrincipale implements Initializable {
     @FXML
     private ObserverButton genererFeuilleBouton;
 
-    /**
-     * Contient tous les points graphiques actuellement afficher grâce à leur
-     * id, et pour chaque intersection, ses arcs graphiques ainsi que
-     * l'intersection ciblée
-     */
-    private Map<Integer, Pair<Ellipse, Collection<Integer>>> intersectionsGraphiques = new HashMap<Integer, Pair<Ellipse, Collection<Integer>>>();
 
-    /**
-     * Contient l'échelle X actuelle par rapport à laquelle les intersections
-     * sont affichés
-     */
-    private double echelleXIntersection = 0;
-    /**
-     * Contient l'échelle Y actuelle par rapport à laquelle les intersections
-     * sont affichés
-     */
-    private double echelleYIntersection = 0;
     /**
      * Méthode appelée lors du redimensionnement de la fenêtre. Elle replace les
      * arrêtes du graphe à leur bonne position
@@ -135,7 +98,7 @@ public class VuePrincipale implements Initializable {
                             Number oldValue, Number newValue) {
 
             canvasGraphique.getChildren().clear();
-            afficherPlan();
+            vueGraphique.afficherPlan();
         }
 
     };
@@ -165,7 +128,7 @@ public class VuePrincipale implements Initializable {
             if (messageErreur != null)
                 ouvrirAlerteXML(messageErreur, file.getName());
             else {
-                construireGraphe(controleurInterface.getPlanDeVille());
+                vueGraphique.construireGraphe(controleurInterface.getPlanDeVille());
 
                 //activation de menu element graphique qui permet de charger un fichier de livraison
                 //(Ici on peut le faire sans appel observeur, parce qu' a parti d'ici on possede toujours un plan valide.)
@@ -193,7 +156,7 @@ public class VuePrincipale implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+    	vueGraphique = new VueGraphiqueAideur(canvasGraphique);
     }
 
     @FXML
@@ -221,142 +184,6 @@ public class VuePrincipale implements Initializable {
     @FXML
     void clic_supprimerLivraison() {
     	controleurInterface.cliqueOutilSupprimer();
-    }
-
-    /**
-     * Construit et affiche le graphe du plan de la ville sur le canvas
-     * graphique de la fenêtre
-     *
-     * @param plan Le plan de la ville, chargée par le couche controleur et
-     *             persistance
-     */
-    private void construireGraphe(PlanDeVille plan) {
-        canvasGraphique.getChildren().clear();
-
-        Map<Integer, Intersection> toutesIntersections = plan.getIntersections();
-        intersectionsGraphiques = new HashMap<Integer, Pair<Ellipse, Collection<Integer>>>();
-
-        echelleXIntersection = 0;
-        echelleYIntersection = 0;
-        // Construction de toutes les intersections
-        for (Intersection i : toutesIntersections.values()) {
-            echelleXIntersection = Math.max(echelleXIntersection, i.getX());
-            echelleYIntersection = Math.max(echelleYIntersection, i.getY());
-
-            Collection<Integer> destinations = new ArrayList<Integer>(); // Tous les tronçons de destinations
-            destinations.addAll(i.getTroncons().keySet());
-
-            intersectionsGraphiques.put(i.getId(),
-                    new Pair<Ellipse, Collection<Integer>>(construireEllipse(i), destinations));
-        }
-
-        afficherPlan();
-    }
-
-    /**
-     * Construit un point graphique pour une intersection du plan de la ville
-     *
-     * @param i L'intersection
-     * @return Le point du graphe, à sa position du fichier XML du plan de la
-     * ville
-     */
-    private Ellipse construireEllipse(Intersection i) {
-        Ellipse intersection = new Ellipse(i.getX(), i.getY(), DIAMETRE_INTERSECTION, DIAMETRE_INTERSECTION);
-        intersection.setFill(Color.YELLOW);
-        return intersection;
-    }
-
-    /**
-     * Affiche tous les points du plan et met à jour la taille du canvas
-     * graphique. Les points sont toujours affichés par rapport : (leur taille
-     * initiale dans le fichier XML / la plus grande taille dans le fichier XML)
-     * => (la nouvelle taille / la taille du canvas)
-     */
-    private void afficherPlan() {
-        // Affichage (+ mise à l'échelle) des intersections
-        for (Pair<Ellipse, Collection<Integer>> pair : intersectionsGraphiques.values()) {
-            afficherEllipse(pair.getKey());
-        }
-
-        // Affichage (+ mise à l'échalle) des tronçons
-        for (Pair<Ellipse, Collection<Integer>> pair : intersectionsGraphiques.values()) {
-            for (int destination : pair.getValue()) {
-                afficherTroncon(pair.getKey(), intersectionsGraphiques.get(destination).getKey());
-            }
-        }
-
-        // Mise à jour de l'échelle sur laquelle sont affichées les intersections du plan
-        echelleXIntersection = 0;
-        echelleYIntersection = 0;
-        for (Pair<Ellipse, Collection<Integer>> pair : intersectionsGraphiques.values()) {
-            echelleXIntersection = Math.max(echelleXIntersection, pair.getKey().getCenterX());
-            echelleYIntersection = Math.max(echelleYIntersection, pair.getKey().getCenterY());
-        }
-
-        // Put nodes to the front
-        for (Node n : canvasGraphique.getChildrenUnmodifiable()) {
-            if (n instanceof Ellipse)
-                n.toFront();
-        }
-    }
-
-    /**
-     * Affiche une ellipse sur l'interface, en fonction de la taille actuelle
-     *
-     * @param e Le point à afficher
-     */
-    private void afficherEllipse(Ellipse e) {
-        double newX = e.getCenterX() * canvasGraphique.getWidth() / (echelleXIntersection + MARGE_INTERSECTION);
-        double newY = e.getCenterY() * canvasGraphique.getHeight() / (echelleYIntersection + MARGE_INTERSECTION);
-
-        e.setCenterX(newX);
-        e.setCenterY(newY);
-
-        canvasGraphique.getChildren().add(e);
-    }
-
-    /**
-     * Affiche le lien entre les deux intersections dans une direction
-     *
-     * @param debut Le point de début
-     * @param cible Le point de destination
-     */
-    private void afficherTroncon(Ellipse debut, Ellipse cible) {
-        double p1X = debut.getCenterX();
-        double p1Y = debut.getCenterY();
-        double p2X = cible.getCenterX();
-        double p2Y = cible.getCenterY();
-
-        Line ligne = new Line(p1X, p1Y, p2X, p2Y);
-        ligne.setStroke(Color.ORANGE);
-
-        canvasGraphique.getChildren().add(ligne);
-
-        // Création flèche
-        double longueur = 8 * Math.sqrt(3);
-        double demiLongueur = 4;
-
-        double Ux = p2X - p1X;
-        double Uy = p2Y - p1Y;
-
-        double distance = Math.sqrt((Ux * Ux) + (Uy * Uy));
-
-        Ux /= distance;
-        Uy /= distance;
-
-        double Vx = -Uy;
-        double Vy = Ux;
-
-        double extremiteFleche1X = p2X - longueur * Ux + demiLongueur * Vx;
-        double extremiteFleche1Y = p2Y - longueur * Uy + demiLongueur * Vy;
-        double extremiteFleche2X = p2X - longueur * Ux - demiLongueur * Vx;
-        double extremiteFleche2Y = p2Y - longueur * Uy - demiLongueur * Vy;
-
-        Polygon fleche = new Polygon(p2X, p2Y, extremiteFleche1X, extremiteFleche1Y, extremiteFleche2X,
-                extremiteFleche2Y);
-        fleche.setFill(Color.RED);
-
-        canvasGraphique.getChildren().add(fleche);
     }
 
     /**
