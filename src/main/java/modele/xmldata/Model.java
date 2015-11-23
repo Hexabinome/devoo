05d3b5577e7942eb206f335e83df8dd5e1b3da58
maxou42 <maxinsa42@gmail.com>
2015-11-23 09:56:17 +0100

    preparing planned timestamp for livraison

--- src/main/java/modele/xmldata/Model.java
+++ src/main/java/modele/xmldata/Model.java
@@ -25,18 +25,6 @@ public class Model implements ModelLecture
         this.demande = demande;
     }
 
-    /*
-     @Override
-     public Graphe getGraphe()
-     {
-     return graphe;
-     }
-
-     public void setGraphe(Graphe graphe)
-     {
-     this.graphe = graphe;
-     }
-     */
     @Override
     public PlanDeVille getPlan()
     {
@@ -74,6 +62,9 @@ public class Model implements ModelLecture
         // apres avoir calcule le graphe il faut appeler TSP ici.
         tsp = new TSP1();
         tsp.chercheSolution(1000, graphe);
+
+        //des que le TSP a fini il faut stoquer les horaires de passage dans l'objet demande
+        remplirHoraires();
     }
 
     @Override
@@ -81,39 +72,47 @@ public class Model implements ModelLecture
     {
         List<List<Integer>> tournee = new LinkedList<>();
         int compteurDesLivraisons = 0;
-        
+
         /**
          * pour toutes les fentres de la demande:
          */
         int livraisonDepart;
         int livraisonArrivee = tsp.getSolution(compteurDesLivraisons++);
         Iterator<Fenetre> iter = demande.getFenetres().iterator();
-        while(iter.hasNext())
-        {
+        while (iter.hasNext()) {
             List<Integer> routePartielle = new LinkedList<>();
-            
+
             iter.next();
             int livraisonsFenetre = iter.next().getLivraisons().size();
             /**
-             * demander TSP a quelle ordre on doit parcourir les destinations de cette fenetre
+             * demander TSP a quelle ordre on doit parcourir les destinations de
+             * cette fenetre
              */
-            while(livraisonsFenetre > 0)
-            {
+            while (livraisonsFenetre > 0) {
                 livraisonsFenetre--;
                 livraisonDepart = livraisonArrivee;
                 livraisonArrivee = tsp.getSolution(compteurDesLivraisons);
-                
+
                 // demander Graphe du chemin pour passer du depart a l'arrivee
                 Chemin chemin = graphe.getChemin(livraisonDepart, livraisonDepart);
-                for(Troncon troncon: chemin.getTroncons())
-                {
+                for (Troncon troncon : chemin.getTroncons()) {
                     routePartielle.add(troncon.getIdDestination());
                 }
-                
+
             }
             tournee.add(routePartielle);
         }
         return tournee;
+    }
+
+    private void remplirHoraires()
+    {
+        //TODO: replace mock implementation by data actually derived from TSP sollution
+        demande.getFenetres().stream().forEach((fenetre) -> {
+            fenetre.getLivraisons().values().stream().forEach((livraison) -> {
+                livraison.setHoraireDePassage((int)(Math.random() * 3600));
+            });
+        });
     }
 
 }
