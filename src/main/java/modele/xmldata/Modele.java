da7a761917be61d9649669d610e512f725bc9530
maxou42 <maxinsa42@gmail.com>
2015-11-28 14:01:01 +0100

    massive refactoring modele (splitting up too long methods, preparing add/delet/change of livraisons

--- src/main/java/modele/xmldata/Modele.java
+++ src/main/java/modele/xmldata/Modele.java
@@ -19,14 +19,19 @@ public class Modele implements ModeleLecture
     private final Demande demande;
     private GrapheRealisation graphe;
     private TSP tsp;
-    private List<List<Integer>> tournee;
+
+    //cette liste des listes stoque pour chque fenetre les livraisons a effecturer dans une tournee calculle par TSP (sans l'entrepot au debout et a la fin de sla tournee)
+    private List<List<Livraison>> livraisonTournee;
+
+    //cette liste des listes stoque pour chque fenetre les intersetions a parcourir dans une tournee calculle par TSP (incluant l'entrepot au debout et a la fin de sla tournee)
+    private List<List<Integer>> intersectionTournee;
 
     public Modele(PlanDeVille plan, Demande demande)
     {
         this.plan = plan;
         this.demande = demande;
         tsp = null;
-        tournee = null;
+        intersectionTournee = null;
     }
 
     public void setGraphe(GrapheRealisation graphe)
@@ -110,12 +115,24 @@ public class Modele implements ModeleLecture
         // apres avoir calcule le graphe il faut qu'on appele TSP ici.
         tsp = new TSP1();
         tsp.chercheSolution(1000, graphe);
-        tournee = creerIntersectionTournee();
 
-        //des que le TSP a fini il faut stoquer les horaires de passage dans l'objet demande
+        //stoque l'ordre des livraisons (calcule par TSP) dans le modele
+        creerLivraisonTournee();
+
+        //creer a parti des listes des livraisonsid, la tournee (granularite intersecitons)
+        intersectionTournee = creerIntersectionTournee();
+        //egalement calculer les horaires de passage pour chaque livraison
         remplirHoraires();
     }
 
+    /**
+     * Cette methode sert a creer une liste des livraionsID dans l'ordre
+     * determine par TSP, pour une fenetre.
+     *
+     * @param fenetre
+     * @param indiceDebutSolutionTsp
+     * @return
+     */
     private List<Livraison> getLivraisonFromSolutionTsp(Fenetre fenetre, int indiceDebutSolutionTsp)
     {
         List<Livraison> listLivraison = new ArrayList<>();
@@ -128,43 +145,73 @@ public class Modele implements ModeleLecture
         return listLivraison;
     }
 
-    private List<List<Integer>> creerIntersectionTournee()
+    /**
+     * Cette methode convert le resultat du TSP dans une liste des listes dont
+     * chaque liste interne represente les livraisons a effectuer dans une
+     * fenetre. L'ordre des livraisons correspond a l'ordre determine par TSP.
+     *
+     * Cette methode doit etre appele apres que une seul fois (directement apres
+     * le fin du calcule TSP). On n'a plus besoin de cette methode quand
+     * l'utilisateur decide de manipuler la tournee calculle a la main.
+     */
+    private void creerLivraisonTournee()
     {
         if (tsp == null)
-            return null;
+            throw new RuntimeException("TSP n'a pas encore calcule la tournee");
 
-        tournee = new LinkedList<>();
+        livraisonTournee = new LinkedList<>();
 
         // compteur pour iterer sur la solution cree par TSP
-        int compteurSolutionTSP = 1;
-
+        int compteurSolutionTSP = 0;
         List<Fenetre> listFenetres = new LinkedList<>();
-
         listFenetres.addAll(demande.getFenetres());
-        Livraison depart = listFenetres.get(0).getListeLivraisons().values().iterator().next();
-        Livraison entrepot = depart;
 
-        // pour chaque fenetre... (sauf le premier qui contient que l'entrepot, on l'enleve)
-        listFenetres.remove(0);
+        //ajouter les livraisons de chaque fenetre dans l'ordre calcule par TSP
         for (Fenetre fenetre : listFenetres) {
 
             List<Livraison> fenetreTspLivraisons = getLivraisonFromSolutionTsp(fenetre, compteurSolutionTSP);
             compteurSolutionTSP += fenetreTspLivraisons.size();
-
-            List<Integer> sousTournee = creerSourTournee(depart, fenetreTspLivraisons);
-            depart = fenetreTspLivraisons.get(fenetreTspLivraisons.size() - 1);
-
-            //ajouter la liste cree pour cette fenetre a la liste principale
-            tournee.add(sousTournee);
+            livraisonTournee.add(fenetreTspLivraisons);
         }
 
-        //ajouter une dernier fenetre fictive pour retourner a l'entrepot
-        List retourEntrepotFenetre = new LinkedList<>();
-        retourEntrepotFenetre.add(entrepot);
-        List<Integer> retourEntrepotSousTournee = creerSourTournee(depart, retourEntrepotFenetre);
-        tournee.add(retourEntrepotSousTournee);
+        //ajouter une derniere fentre avec une seule livraison (retour a l'entrepot)
+        List<Livraison> retourEntrepot = new LinkedList<>();
+        //recuperer la livraison fantastique du premier fenetre (seul livraison au premiere fenetre = entrepot)
+        retourEntrepot.add(listFenetres.get(0).getListeLivraisons().values().iterator().next());
+        livraisonTournee.add(retourEntrepot);
+    }
 
-        return tournee;
+    /**
+     * Cette methode utilise la liste des listes des livraisons pour calculer
+     * une tournee (granularite intersection). Ca nous permet de facilement
+     * mettre ajour la tournee si le utilisateur decide d'ajouter une nouvelle
+     * livraisons.
+     */
+    private List<List<Integer>> creerIntersectionTournee()
+    {
+        intersectionTournee = new LinkedList<>();
+
+        //depart est l'entrepot (seul livraison dans premiere fenetre)
+        Livraison depart = livraisonTournee.get(0).get(0);
+
+        boolean entrepot = true;
+        for (List<Livraison> fenetre : livraisonTournee) {
+            if (entrepot)
+                //on doit sauter la premiere fenetre (l'algo desous utilise toujours une fenetre et sa fenetre precedente)
+                entrepot = false;
+            else {
+                List<Integer> sousTournee = creerSourTournee(depart, fenetre);
+
+                //depart de prochaine fenetre est la derniere livraison de cette fenetre
+                depart = fenetre.get(fenetre.size() - 1);
+
+                //ajouter la liste cree pour cette fenetre a la liste principale
+                intersectionTournee.add(sousTournee);
+            }
+
+        }
+
+        return intersectionTournee;
     }
 
     private List<Integer> creerSourTournee(Livraison depart, List<Livraison> sousTourneeLivraisons)
@@ -201,12 +248,12 @@ public class Modele implements ModeleLecture
     @Override
     public List<List<Integer>> getTournee()
     {
-        if (tournee == null)
+        if (intersectionTournee == null)
             return null;
 
         List<List<Integer>> inmodifiableTournee = new LinkedList<>();
 
-        for (List<Integer> sousTournee : tournee) {
+        for (List<Integer> sousTournee : intersectionTournee) {
             inmodifiableTournee.add(Collections.unmodifiableList(sousTournee));
         }
         return Collections.unmodifiableList(inmodifiableTournee);
