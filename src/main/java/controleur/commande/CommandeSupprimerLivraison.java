7707c9da725158a3790c067fbcf1dd68f77d6ca6
maxou42 <maxinsa42@gmail.com>
2015-11-29 12:09:43 +0100

    No more exception when trying to remove last livraison of a fenetre... Controleur now checks first if user interaction is allowed

--- src/main/java/controleur/commande/CommandeSupprimerLivraison.java
+++ src/main/java/controleur/commande/CommandeSupprimerLivraison.java
@@ -1,31 +1,40 @@
 package controleur.commande;
 
 import controleur.ControleurDonnees;
+import modele.xmldata.Fenetre;
 import modele.xmldata.Livraison;
 
 /**
  * Représente une commande de suppression de livraisons
  */
-public class CommandeSupprimerLivraison implements Commande {
-
+public class CommandeSupprimerLivraison implements Commande
+{
 
     private final ControleurDonnees controleurDonnees;
 
     private final Livraison livraisonSupprimee;
 
-    public CommandeSupprimerLivraison(ControleurDonnees controleurDonnees, int idLivraison) {
+    public CommandeSupprimerLivraison(ControleurDonnees controleurDonnees, int idLivraison)
+    {
         this.controleurDonnees = controleurDonnees;
         livraisonSupprimee = this.controleurDonnees.getModele().getDemande().identifierLivraison(idLivraison);
 
     }
 
     @Override
-    public boolean estAnnulable() {
+    public boolean estAnnulable()
+    {
         return true;
     }
 
     @Override
-    public void executer() throws CommandeException {
+    public void executer() throws CommandeException
+    {
+
+        //verifier qu'on a le droit de supprimer la livraison
+        Fenetre f = controleurDonnees.getModele().getDemande().getFenetreDeLivraison(livraisonSupprimee.getId());
+        if (f.getListeLivraisons().size() <= 1)
+            return;
 
         controleurDonnees.getModele().removeLivraison(livraisonSupprimee.getId());
         controleurDonnees.getModele().remplirHoraires();
@@ -35,7 +44,9 @@ public class CommandeSupprimerLivraison implements Commande {
     }
 
     @Override
-    public void annuler() {
+    public void annuler()
+    {
         // TODO : appeler le modele pour rajouter la livraison et ne pas calculer toute la tournee
     }
+
 }
