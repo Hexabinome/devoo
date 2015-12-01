20a4ce875673817181f5f6912cb6b5aa2c540bce
Mohamed Haidara <mohamed.haidara@insa-lyon.fr>
2015-12-01 13:30:09 +0100

    Commentaires dans ObjetVisualisable

--- src/main/java/vue/ObjetVisualisable.java
+++ src/main/java/vue/ObjetVisualisable.java
@@ -2,26 +2,28 @@ package vue;
 
 /**
  * Cette classe permet de visualer une Fenetre (de livraison) ou une livraison sous forme textuelle.
- * Cette visualisation peut se faire de deux façons façons differentes. Ces objets sont aussi Visitable (lors d'un clique
- * par exemple)
+ * Cette visualisation peut se faire de deux façons façons differentes en fonction du type concrêt de l'objet.
  */
 public abstract class ObjetVisualisable {
+
+
     abstract public String afficherCaracteriquesGloable();
 
     abstract public String afficherCaracteriqueSpeciale();
-    
+
     private CouleurTexte couleurDefaut = CouleurTexte.NON_SURBRILLANCE;
-    
+
     public void setCouleurDefaut(CouleurTexte couleur) {
-    	couleurDefaut = couleur;
+        couleurDefaut = couleur;
     }
-    
+
     public CouleurTexte getCouleurDefaut() {
-    	return couleurDefaut;
+        return couleurDefaut;
     }
-    
+
     /**
      * Convertis un temps en seconde en chaine de caractère sous la forme HH:mm:ss
+     *
      * @param tempsEnSeconde temps à convertir
      */
     protected static String convertirEnHeureLisible(int tempsEnSeconde) {
@@ -30,10 +32,13 @@ public abstract class ObjetVisualisable {
         int sec = tempsEnSeconde % 60;
         return String.format("%02d:%02d:%02d", heure, mn, sec);
     }
-    
+
+    /**
+     * Différentes couleurs possibles pour un élément dans la liste.
+     */
     public enum CouleurTexte {
-    	SURBRILLANCE,
-    	NON_SURBRILLANCE,
-    	RETARD;
+        SURBRILLANCE,
+        NON_SURBRILLANCE,
+        RETARD
     }
 }
