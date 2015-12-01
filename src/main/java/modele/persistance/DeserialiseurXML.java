bd300e121f4d248c0dcfb2858c0d0518a8d32107
Mohamed Haidara <mohamed.haidara@insa-lyon.fr>
2015-12-01 20:04:16 +0100

    Commentaires dans la partie XML

--- src/main/java/modele/persistance/DeserialiseurXML.java
+++ src/main/java/modele/persistance/DeserialiseurXML.java
@@ -22,21 +22,16 @@ import java.util.List;
  * DeserialiseurXML comme son nom l'indique permet de déserialiser un fichier
  * xml en créant les objets correspondant. Dans notre cas, elle déserialisera le
  * Plan de la ville et la demande de livraison. Cette classe utilise des schémas
- * XML (XSD) pour valider les fichiers XML en entrée. L'utilisation
- * d'InputStream se justifie par le fait que en créant un du projet jar, on ne
- * pourra pas accéder aux schémas sous la forme de File sans toucher au système
- * de fichier du système.
+ * XML (XSD) pour valider les fichiers XML en entrée.
  *
  * @author Mohamed El Mouctar HAIDARA
  */
-public class DeserialiseurXML
-{
+public final class DeserialiseurXML {
 
     /**
      * Constructeur privé. Pas besoin d'instancier la class
      */
-    private DeserialiseurXML()
-    {
+    private DeserialiseurXML() {
     }
 
     /**
@@ -44,24 +39,25 @@ public class DeserialiseurXML
      *
      * @param planXML le fichier xml répresentant le plan de la ville.
      * @throws JDOMException Problème survenu lors de du parsing
-     * @throws IOException Problème survenu lors de la lecture du fichier
-     * @throws SAXException Problème survenu lors de la validation par le schéma
-     * XSD
+     * @throws IOException   Problème survenu lors de la lecture du fichier
+     * @throws SAXException  Problème survenu lors de la validation par le schéma
+     *                       XSD
      */
     public static PlanDeVille ouvrirPlanDeVille(InputStream planXML)
-            throws JDOMException, IOException, SAXException, ExceptionXML
-    {
+            throws JDOMException, IOException, SAXException, ExceptionXML {
+        // Chargement du validateur xsd et validation
+        // La même instance du validateur ne peut pas être utilisée deux fois d'où la création à chaque nouvel chargement
         InputStream XSDPLAN = ClassLoader.getSystemResourceAsStream("xsd/validateurPlan.xsd");
         Document document = validerFichierXML(XSDPLAN, planXML);
-        // System.out.println(fichierValidationLivraisons);
+
         PlanDeVille planDeVille = new PlanDeVille();
 
+        // Parsing
         Element elementPere = document.getRootElement();
 
         // Récuperation des Intersections
         List<Element> intersectionList = elementPere.getChildren("Noeud");
         for (Element e : intersectionList) {
-            //System.out.println(e.getAttribute("id")+" - " + e.getAttribute("x") +" - " + e.getAttribute("y"));
             int idIntersection = e.getAttribute("id").getIntValue();
             int xIntersection = e.getAttribute("x").getIntValue();
             int yIntersection = e.getAttribute("y").getIntValue();
@@ -80,6 +76,7 @@ public class DeserialiseurXML
                 // Conversion des String en float
                 float vitesse = Float.parseFloat(vitesseString);
                 float longueur = Float.parseFloat(longueurString);
+
                 int idIntersectionDestination = elementTroncon.getAttribute("idNoeudDestination").getIntValue();
                 if (idIntersectionDestination == idIntersection)
                     throw new ExceptionXML("Impossible de charger le plan : une intersection plointe vers elle même");
@@ -91,7 +88,6 @@ public class DeserialiseurXML
             }
             planDeVille.addInstersection(intersection);
         }
-        //  System.out.println(planDeVille);
         return planDeVille;
     }
 
@@ -100,36 +96,34 @@ public class DeserialiseurXML
      *
      * @param planXML le fichier xml répresentant le plan de la ville.
      * @throws JDOMException Problème survenu lors de du parsing
-     * @throws IOException Problème survenu lors de la lecture du fichier
-     * @throws SAXException Problème survenu lors de la validation par le schéma
-     * XSD
+     * @throws IOException   Problème survenu lors de la lecture du fichier
+     * @throws SAXException  Problème survenu lors de la validation par le schéma
+     *                       XSD
      */
     public static PlanDeVille ouvrirPlanDeVille(File planXML)
-            throws JDOMException, IOException, SAXException, ExceptionXML
-    {
+            throws JDOMException, IOException, SAXException, ExceptionXML {
         InputStream inputStream = new FileInputStream(planXML);
         return ouvrirPlanDeVille(inputStream);
     }
 
     /**
-     * Construis le plan des livraisons à partir d'un fichier xml.
+     * Construit la demande de livraisons à partir d'un fichier xml.
      *
      * @param livraisonXml le fichier xml répresentant la demande de livraison
-     * @param planDeVille le plan de la ville préalablement chargé
+     * @param planDeVille  le plan de la ville préalablement chargé
      * @throws JDOMException Problème survenu lors de du parsing
-     * @throws IOException Problème survenu lors de la lecture du fichier
+     * @throws IOException   Problème survenu lors de la lecture du fichier
      */
     public static Demande ouvrirLivraison(InputStream livraisonXml, final PlanDeVille planDeVille)
-            throws SAXException, IOException, JDOMException, ParseException, ExceptionXML
-    {
+            throws SAXException, IOException, JDOMException, ParseException, ExceptionXML {
 
-        InputStream XSDLIVRAISON = ClassLoader.getSystemResourceAsStream(
-                "xsd/validateurLivraisons.xsd");
+        // Chargement du validateur xsd et validation
+        // La même instance du validateur ne peut pas être utilisée deux fois d'où la création à chaque nouvel chargement
+        InputStream XSDLIVRAISON = ClassLoader.getSystemResourceAsStream("xsd/validateurLivraisons.xsd");
         Document document = validerFichierXML(XSDLIVRAISON, livraisonXml);
         Element journeeType = document.getRootElement();
 
         // Récuperation de l'entrepot
-        // System.out.println("entrepot : " + journeeType.getChild("Entrepot").getAttribute("adresse").getIntValue());
         int idEntrepot = journeeType.getChild("Entrepot").getAttribute("adresse").getIntValue();
         Intersection intersectionEntrepot = planDeVille.getIntersection(idEntrepot);
         if (intersectionEntrepot == null)
@@ -144,11 +138,8 @@ public class DeserialiseurXML
         Livraison livraison;
 
         /**
-         * les id's des livraisons stoques dans les xml sont unique dans le
-         * "scope" d'une fenetre. Par contre quand on a plusieurs fenetres, ces
-         * IDs ne sont plus unique. Du coup pour avoirt des ids uniques dans le
-         * modele un utilise l'id ET l'index de la fenetre pour creer une
-         * livraison unique.
+         * Les ids des livraisons stockées dans les fichiers XML sont uniques par fenêtre.
+          Du coup pour avoir des ids uniques dans le modele un utilise l'id ET l'index de la fenetre pour creer une livraison.
          */
         int fenetreCompteur = 0;
         for (Element elementFenetre : listePlage) {
@@ -167,16 +158,13 @@ public class DeserialiseurXML
             List<Element> listeLivraison = elementFenetre.getChild("Livraisons").getChildren();
 
             for (Element elementLivraison : listeLivraison) {
-                /* System.out.println("id : " + elementLivraison.getAttribute("id").getIntValue());
-                 System.out.println("client : " + elementLivraison.getAttribute("client").getIntValue());
-                 System.out.println("adresse : " + elementLivraison.getAttribute("adresse").getIntValue()); */
 
                 int idLivraison = elementLivraison.getAttribute("id").getIntValue();
 
                 /**
-                 * on ajoute 10000 l'index du fenetre. Comme ca les ids
-                 * restent unique (supposant qu'on a moins de 10000 livraisons
-                 * pas fenetre, mais ca semble realiste)
+                 * on ajoute 10000 l'index de fenêtre. Comme ça les ids
+                 * restent uniques (supposant qu'on a moins de 10000 livraisons
+                 * par fenêtre, ça semble réaliste)
                  */
                 idLivraison += fenetreCompteur * 10000;
                 int idClient = elementLivraison.getAttribute("client").getIntValue();
@@ -199,15 +187,14 @@ public class DeserialiseurXML
     }
 
     /**
-     * Construis le plan des livraisons à partir d'un fichier xml.
+     * Construis la demande de livraisons à partir d'un fichier xml.
      *
      * @param planDeVille le plan de la ville préalablement chargé
      * @throws JDOMException Problème survenu lors de du parsing
-     * @throws IOException Problème survenu lors de la lecture du fichier
+     * @throws IOException   Problème survenu lors de la lecture du fichier
      */
     public static Demande ouvrirLivraison(File livraisonXml, final PlanDeVille planDeVille)
-            throws SAXException, IOException, JDOMException, ParseException, ExceptionXML
-    {
+            throws SAXException, IOException, JDOMException, ParseException, ExceptionXML {
 
         InputStream inputStream = new FileInputStream(livraisonXml);
 
@@ -217,17 +204,16 @@ public class DeserialiseurXML
     /**
      * Valide un fichier XML avec le fichier XSD fourni.
      *
-     * @param xsdStream Le schéma XSD à utiliser.
+     * @param xsdStream  Le schéma XSD à utiliser.
      * @param fichierXML Le fichier XML à valider.
      * @return Renvoie le document correspondant si la validation est effective.
      * @throws JDOMException Si la validation du XML a échoué.
-     * @throws IOException S'il y a eu une erreur de lecture.
+     * @throws IOException   S'il y a eu une erreur de lecture.
      */
     private static Document validerFichierXML(InputStream xsdStream, InputStream fichierXML)
-            throws JDOMException, IOException
-    {
+            throws JDOMException, IOException {
 
-        Document document = null;
+        Document document;
         XMLReaderJDOMFactory factory = new XMLReaderXSDFactory(new StreamSource(xsdStream));
         SAXBuilder saxBuilder = new SAXBuilder(factory);
         document = saxBuilder.build(fichierXML);
@@ -241,8 +227,7 @@ public class DeserialiseurXML
      * @param heureMnSec Chaine de caractère à convertir
      * @return
      */
-    private static int convertirHeureEnSeconde(String heureMnSec)
-    {
+    private static int convertirHeureEnSeconde(String heureMnSec) {
         String[] decoupage = heureMnSec.split(":");
 
         int heure = Integer.parseInt(decoupage[0]);
--- src/main/java/modele/persistance/ExceptionXML.java
+++ src/main/java/modele/persistance/ExceptionXML.java
@@ -1,11 +1,13 @@
 package modele.persistance;
 
 /**
- * Created by elmhaidara on 24/11/15.
+ * Cette répresente une erreur survenue lors du parsing d'un plan de ville ou d'une demande de livraison.
+ * Elle ne concerne pas les erreurs de mal formation du document mais certaines erreurs liées à la semantique (une intersection pointe sur elle-même par exemple).
+ * @author Mohamed El Mouctar HAIDARA
  */
 public class ExceptionXML extends Exception {
 
-    public ExceptionXML(String message){
+    public ExceptionXML(String message) {
         super(message);
     }
 }
